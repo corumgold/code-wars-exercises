0269d5b73aa09470f55387a3aa74e729448708ca
Corum Gold <goldcorum@gmail.com>
2022-05-31 22:21:20 -0400

    Start looking for solution

---
+++ find-the-odd-int.js
@@ -0,0 +1,10 @@
+function findOdd(A) {
+   debugger
+    let counter = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] //counts digits 0 through 9
+    for (let i = 0; i < A.length; i++){
+      counter[A[i]]++;
+    }
+    return 0;
+  }
+
+  
