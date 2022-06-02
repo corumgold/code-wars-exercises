a60a2bce1d06e67f6fb5fafd436683ed9c77cce3
Corum Gold <goldcorum@gmail.com>
2022-06-01 22:27:03 -0400

    Finish exercise for more simpler problems

---
+++ find-parity-outlier.js
@@ -0,0 +1,27 @@
+function findOutlier(integers) {
+    debugger;
+    let oddsCount = 0
+    let evensCount = 0
+    for (let i = 0; i < integers.length; i++) {
+        if (integers[i] % 2 === 0) {
+            evensCount++
+        } else {
+            oddsCount++
+        }
+    }
+    if (oddsCount > evensCount) {
+        for (let i = 0; i < integers.length; i++) {
+            if (integers[i] % 2 === 0) {
+                return integers[i]
+            }
+        }
+    } else {
+        for (let i = 0; i < integers.length; i++) {
+            if (integers[i] % 2 === 1) {
+                return integers[i]
+            }
+        }
+    }
+}
+
+findOutlier([2, 6, 8, 10, 3])
