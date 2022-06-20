0a8d18a76e8abeb29aef488b7f09730912ff14df
Corum Gold <goldcorum@gmail.com>
2022-06-20 14:06:42 -0400

    Markup simple piglatin exercise

--- simple_piglatin.js
+++ simple_piglatin.js
@@ -8,14 +8,18 @@
 function pigIt(str) {
     debugger;
     let punc = "!?.,"
+    //pull words apart so we can deal with them
     let arr = str.split(' ')
+    //create empty array to push our "pigged" words
     let pigArr = [];
     for (let i = 0; i < arr.length; i++) {
         let word = arr[i];
+        //if the word is a punctuation mark, just push it to the pig array
         if (punc.indexOf(word) !== -1) {
             pigArr.push(word);
         }
         else {
+            //if it's a regular word, "piggify" it and push it to the pig arr
             let pigged = word.slice(1) + word.slice(0, 1) + "ay";
             pigArr.push(pigged);
         }
