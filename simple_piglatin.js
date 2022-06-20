e4558d62d6a22378c40fbbb54fbb8ca4d9d3d4cf
Corum Gold <goldcorum@gmail.com>
2022-06-20 14:05:00 -0400

    Finish simple piglatin exercise

--- simple_piglatin.js
+++ simple_piglatin.js
@@ -5,6 +5,21 @@
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
 
-function pigIt(str){
-    //Code here
-  }
+function pigIt(str) {
+    debugger;
+    let punc = "!?.,"
+    let arr = str.split(' ')
+    let pigArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let word = arr[i];
+        if (punc.indexOf(word) !== -1) {
+            pigArr.push(word);
+        }
+        else {
+            let pigged = word.slice(1) + word.slice(0, 1) + "ay";
+            pigArr.push(pigged);
+        }
+    } return pigArr.join(' ');
+}
+
+pigIt("Hey there my name is Cory !")
