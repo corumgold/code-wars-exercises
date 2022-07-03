6548c588a0e381b9004b8d9bddc3aa3cb9a0bb3f
Corum Gold <goldcorum@gmail.com>
2022-07-03 11:27:48 -0400

    Finish duplicate encoder exercise

--- duplicate_encoder.js
+++ duplicate_encoder.js
@@ -1,17 +1,28 @@
-// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" 
-// if that character appears only once in the original string, or ")" if that character appears more than once in the original string. 
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "("
+// if that character appears only once in the original string, or ")" if that character appears more than once in the original string.
 // Ignore capitalization when determining if a character is a duplicate.
 
 // Examples
 // "din"      =>  "((("
 // "recede"   =>  "()()()"
 // "Success"  =>  ")())())"
-// "(( @"     =>  "))((" 
+// "(( @"     =>  "))(("
 
 // Notes
-// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", 
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX",
 // the "XXX" is the expected result, not the input!
 
-function duplicateEncode(word){
-    
+function duplicateEncode(word) {
+  const smallWord = word.toLowerCase();
+  let encoded = "";
+  for (let i = 0; i < smallWord.length; i++) {
+    let char = smallWord[i];
+    let charCount = 0;
+    for (let j = 0; j < smallWord.length; j++) {
+      if (smallWord[j] === char) charCount++;
+    }
+    if (charCount === 1) encoded += "(";
+    else encoded += ")";
+  }
+  return encoded;
 }
