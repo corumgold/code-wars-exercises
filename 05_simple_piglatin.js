5df341d7be7510a408730a726444aa3c75d01a1a
Corum Gold <goldcorum@gmail.com>
2022-07-06 22:24:19 -0400

    Finish simple pig latin -- 2nd

---
+++ 05_simple_piglatin.js
@@ -0,0 +1,24 @@
+//Link: https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
+
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+// Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+//solveCount = 2;
+
+function pigIt(str) {
+  let puncs = ".,!?";
+  return str
+    .split(" ")
+    .map((word) => {
+      if (puncs.indexOf(word) === -1) {
+        return word.slice(1) + word.slice(0, 1) + "ay";
+      } else return word;
+    })
+    .join(" ");
+}
+
+pigIt("Hey there my name is Cory !");
