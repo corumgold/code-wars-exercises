11b0a3575a9f4fd78276b027459773e9c65679d1
Corum Gold <goldcorum@gmail.com>
2022-07-19 12:02:49 -0400

    Solve pete the baker -- 2nd

--- 05_pete_the_baker.js
+++ 05_pete_the_baker.js
@@ -1,6 +1,6 @@
 //source: https://www.codewars.com/kata/525c65e51bf619685c000059
 
-// Pete likes to bake some cakes.He has some recipes and ingredients.Unfortunately he is not good in maths. 
+// Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths.
 // Can you help him to find out, how many cakes he could bake considering his recipes ?
 
 // Write a function cakes(), which takes the recipe(object) and the available ingredients(also an object)
@@ -10,10 +10,21 @@
 // Examples:
 
 // // must return 2
-// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
 // // must return 0
-// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); 
+// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+
+//solveCount = 1;
 
 function cakes(recipe, available) {
-    
-  }
+  return Math.floor(
+    Object.keys(recipe)
+      .reduce((acc, curr) => {
+        let ratio = available[curr] / recipe[curr];
+        if (ratio) acc.push(ratio);
+        else acc.push(0);
+        return acc;
+      }, [])
+      .sort((a, b) => a - b)[0]
+  );
+}
